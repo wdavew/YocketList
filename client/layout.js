0eb20262cb676078dc52edc3b953015e822ceed3
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-31 17:24:13 -0700

    Moved routing here and eliminated path to playerapp, which was merged into queueapp.

--- client/layout.js
+++ client/layout.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router';
-
+import { Router, Route, Link, hashHistory } from 'react-router';
+import QueueApp from './components/queueapp';
 /**
  * Layout is the react-router component that defines the structure of the page.
  * this.props.children changes based on the current route.
  */
 class Layout extends React.Component {
  render() {
-    return (
-      <div>
-        <h1>Yocket List</h1>
-        <ul role="nav">
-          <li><Link to="player">PlayerApp</Link></li>
-          <li><Link to="queue">QueueApp</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
+   return (
+     <Router history={hashHistory}>
+       <Route path="/queue" component={QueueApp} />
+     </Router>
     )
   }
 }
