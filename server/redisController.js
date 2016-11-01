7804e3ad3ab4b8e0b906894ff9afa48a8aec6b5c
David Wilson <w.dave.w@gmail.com>
2016-10-31 21:31:43 -0700

    Add additional redis controller functionality
    - Add additional unit tests

--- server/redisController.js
+++ server/redisController.js
@@ -40,16 +40,24 @@ let red = {
 		.catch(err => console.log('error in return video'));
 	},
 
-//
+	// creates a room if it does not already exist; throws error if the room exists
 	createRoom: function(roomNum){
-		roomExists(roomNum).then(response => {
-			if (response === 1) client.hset(['rooms', roomNum, 1]);
-			else throw new Error('room already exists');
+		return this.roomExists(roomNum).then(response => {
+			if (!response) {
+				return client.hsetAsync(['rooms', roomNum, 1]);
+			} else throw new Error('room already exists');
 		})
 	},
-	//
+
+	// returns a boolean indicating whether or not the provided room exists
 	roomExists: function(roomNum){
-		return client.hexistsAsync(['rooms', roomNum]);
+		return client.hexistsAsync(['rooms', roomNum])
+		.then(response => Boolean(response));
+	},
+
+	// returns all videos in queue sorted by votes as an array
+	returnQueue: function(roomNum){
+		return client.zrevrangeAsync([`${roomNum}videos`, 0, -1])
 	}
 };
 
