df31c4e2e069fe5495bce6cd7c1d3f6a9dc24809
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-01 16:40:10 -0700

    added params routing to access roomname upon room creation

--- client/components/Queueapp.js
+++ client/components/Queueapp.js
@@ -8,17 +8,15 @@ import io from 'socket.io-client';
 const HOST = "http://localhost:3000";
 
 class QueueApp extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { queues: [] }
+    this.socket = io.connect(HOST);
 
     this.getData = this.getData.bind(this);
-    this.formClick = this.formClick.bind(this);    
+    this.formClick = this.formClick.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
     this.handlePlayerEnd = this.handlePlayerEnd.bind(this);
-
-    this.socket = io.connect(HOST);
-    this.initializeListeners();
   } 
   /**
    * We GET our data here after each render
@@ -26,28 +24,15 @@ class QueueApp extends Component {
   getData() {
     $.get(HOST + "/queue").done(data => this.setState({ queues: data }));
   }
-  /**
-   * This is where the listeners for this.socket go.
-   * on[newData] -> Implies there is a change in data on the backend.
-   *                The callback will make a GET request and update state
-   *                with the new list of Youtube URLs.
-   */
-  initializeListeners() {
-    this.socket.on('newdata', this.getData);
-  }
     /**
    * handleStateChange is an event listener for the react-youtube
    * component's state. The states are as follows:
    * UNSTARTED: -1, ENDED: 0, PLAYING: 1, PAUSED: 2, BUFFERING: 3, CUED: 5
    */
-  createRoom() {
-    this.socket.emit('room', { roomName: 'input' }); // connect to form input field on other page
-  }
-
   handleStateChange(event) {
-    console.log(event.data);
     // CUED was a good option for enabling "auto play" because it waits
-    // until the player is loaded (-1) and then the video is cued ready to play
+    // until the player is loaded (-1) and then the video is cued ready to play.
+    // ENDED allows repeat behavior for last video.
     if (event.data === 5 || event.data === 0) event.target.playVideo();
   }
 /**
@@ -72,16 +57,25 @@ class QueueApp extends Component {
     newQueues.push(link); 
     this.setState({ queues: newQueues });
     $.ajax({
-      url: HOST+"/queue",
+      url: HOST + "/queue",
       type:"POST",
       data: JSON.stringify({ link: link }),
       contentType:"application/json; charset=utf-8",
       dataType:"json",
     });
   }
-
+  /**
+   * This is where the listeners for this.socket go.
+   * on[newData] -> Implies there is a change in data on the backend.
+   *                The callback will make a GET request and update state
+   *                with the new list of Youtube URLs.
+   * on[joiningRoom] -> Implies someone has created a room.
+   *                    The callback will update our roomName in the constructor
+   *                    with the input value typed into the Home createRoom form.
+   */
   componentDidMount() {
     this.getData();
+    this.socket.on('newdata', this.getData);
   }
 
   render() {
@@ -90,7 +84,7 @@ class QueueApp extends Component {
 
     return (
       <div className="youtube-wrapper">
-        <h1>Welcome to QTube!</h1>
+        <h1>Welcome to QTube! Your room is: {this.props.params.roomName}</h1>
         <Form key={'form-key'} formClick={this.formClick} />
         <Youtube videoId={videoUrl} onEnd={this.handlePlayerEnd} onStateChange={this.handleStateChange} />
         <QueueList queues={this.state.queues} />
--- client/layout.js
+++ client/layout.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
-import QueueApp from './components/queueapp';
+import { Router, Route, Link, browserHistory } from 'react-router';
+import QueueApp from './components/Queueapp';
+import Home from './components/Home';
 /**
  * Layout is the react-router component that defines the structure of the page.
- * this.props.children changes based on the current route.
  */
 class Layout extends Component {
  render() {
    return (
-     <Router history={hashHistory}>
-       <Route path="/queue" component={QueueApp} />
+     <Router history={browserHistory}>
+       <Route path="/" component={Home} />
+       <Route path="/queue/:roomName" component={QueueApp} />
      </Router>
-    )
+    );
   }
 }
 
