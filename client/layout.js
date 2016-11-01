ee04df51899f618b4a90824f2bc219e5210d6804
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-31 17:36:48 -0700

    modified ES5 to ES6

--- client/layout.js
+++ client/layout.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { Router, Route, Link, hashHistory } from 'react-router';
 import QueueApp from './components/queueapp';
 /**
  * Layout is the react-router component that defines the structure of the page.
  * this.props.children changes based on the current route.
  */
-class Layout extends React.Component {
+class Layout extends Component {
  render() {
    return (
      <Router history={hashHistory}>
