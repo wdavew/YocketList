f79f47a92e9be0f0277be18a57470484c1255dd5
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-01 16:42:28 -0700

    minor text fixes

--- client/components/Queue.js
+++ client/components/Queue.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Queue = ({ link }) => {
   // This interesting piece of string manipulation takes in a youtube url and turns it into a thumbnail url
-  return <img src={`https://i.ytimg.com/vi/${link.split('=')[1]}/hqdefault.jpg`}></img>
+  return <img src={`https://i.ytimg.com/vi/${link.split('=')[1]}/hqdefault.jpg`}></img>;
 }
 
 export default Queue;
