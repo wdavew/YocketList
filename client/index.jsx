c1cd043503fd5cc942c15c9d44330c8b1936b4f6
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-31 17:23:19 -0700

    made this the only page to render the DOM and passed routing into the Layout component

--- client/index.jsx
+++ client/index.jsx
@@ -1,21 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-import Layout from './layout';
-import PlayerApp from './playerapp';
-import QueueApp from './queueapp';
+import Layout from './Layout';
 
-
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
-      {/* make them children of `App` */}
-      <Route path="player" component={PlayerApp}/>
-      <Route path="queue" component={QueueApp}/>
-    </Route>
-  </Router>,
-  document.getElementById('App'))
-
-
-
-
+ReactDOM.render(<Layout />, document.getElementById('App'))
