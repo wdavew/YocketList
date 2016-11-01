fcb3be8beb2b0112644c851ed3f9c44b581aa1f4
David Wilson <w.dave.w@gmail.com>
2016-11-01 00:52:06 -0700

    Promisify everything

--- server/redisController.js
+++ server/redisController.js
@@ -4,44 +4,46 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 client.on("error", function (err) {
-    console.log("Error " + err);
+	console.log("Error " + err);
 });
 
 let red = {
 	// adds administrator with given roomnum key
-	 addAdmin: function(roomNum, cookie){
-		client.set(`${roomNum}admin`, cookie);
+	 addAdmin: function (roomNum, cookie) {
+		return client.setAsync(`${roomNum}admin`, cookie)
 	},
 
 	//adds a video with the given roomnum key
-	 addVideo: function(roomNum, videoUrl) {
-		client.zadd([`${roomNum}videos`, 0, videoUrl], (err, response) => {
-			if (err) throw err;
-			console.log('added ' + response + ' items.');
-		})
+	 addVideo: function (roomNum, videoUrl) {
+		return client.zaddAsync([`${roomNum}videos`, 0, videoUrl])
+			.catch((err) => {
+				throw err;
+			});
 	},
 
 	//increments score of a video
-	 incScore: function(roomNum, videoUrl) {
-		client.zincrby([`${roomNum}videos`, 1, videoUrl], (err, response) => {
-			if (err) throw err;
-			console.log('added ' + response + ' items.');
-		})
-	},
+	 incScore: function (roomNum, videoUrl) {
+		return client.zincrbyAsync([`${roomNum}videos`, 1, videoUrl])
+			.then(resp => Number(resp))
+			.catch(err => {
+				throw (err)
+			});
+		},
+
 
 	// remove & return 'next' video
 	// will return a promise object that resolves to the video url
-	 returnVideo: function(roomNum){
+	 returnVideo: function (roomNum) {
 		let urlOfNextVideo;
-		return client.zrangeAsync([`${roomNum}videos`, 0, 0])
-		.then(resp => urlOfNextVideo = resp)
-		.then(client.zremrangebyrankAsync([`${roomNum}videos`, 0, 0]))
-		.then(resp => urlOfNextVideo[0])
-		.catch(err => console.log('error in return video'));
+		return client.zrevrangeAsync([`${roomNum}videos`, 0, 0])
+			.then(resp => urlOfNextVideo = resp)
+			.then(client.zremrangebyrankAsync([`${roomNum}videos`, -1, -1]))
+			.then(resp => urlOfNextVideo[0])
+			.catch(err => console.log('error in return video'));
 	},
 
 	// creates a room if it does not already exist; throws error if the room exists
-	createRoom: function(roomNum){
+	createRoom: function (roomNum) {
 		return this.roomExists(roomNum).then(response => {
 			if (!response) {
 				return client.hsetAsync(['rooms', roomNum, 1]);
@@ -50,16 +52,16 @@ let red = {
 	},
 
 	// returns a boolean indicating whether or not the provided room exists
-	roomExists: function(roomNum){
+	roomExists: function (roomNum) {
 		return client.hexistsAsync(['rooms', roomNum])
-		.then(response => Boolean(response));
+			.then(response => Boolean(response));
 	},
 
 	// returns all videos in queue sorted by votes as an array
-	returnQueue: function(roomNum){
+	returnQueue: function (roomNum) {
 		return client.zrevrangeAsync([`${roomNum}videos`, 0, -1])
 	}
 };
 
 
-module.exports = {red};
+module.exports = { red };
