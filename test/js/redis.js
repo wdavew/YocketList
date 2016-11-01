a0711e4a75dec36b521326cb453c2bce8e2cd229
David Wilson <w.dave.w@gmail.com>
2016-11-01 00:52:18 -0700

    Add tests for removeVid method

--- test/js/redis.js
+++ test/js/redis.js
@@ -14,40 +14,42 @@ before(() => {
 })
 describe('addAmin', () => {
   it('should add a roomnumber key with cookie val', () => {
-    redisController.addAdmin('Room 1', 'user 5');
-    return client.getAsync('Room 1admin').should.eventually.equal('user 5');
+    return redisController.addAdmin('Room 1', 'user 5')
+      .then(() => client.getAsync('Room 1admin').should.eventually.equal('user 5'));
   });
 });
 
 describe('addVideo', () => {
-  it('should add a roomnumber key with cookie val', () => {
-    redisController.addVideo('Room 1', 'www.video.com');
-    return client.zrangeAsync('Room 1videos', '0', '0').should.eventually.deep.equal(['www.video.com']);
+  it('should add a video url string to the room key', () => {
+    return redisController.addVideo('Room 1', 'www.video.com')
+      .then(() => client.zrangeAsync('Room 1videos', '0', '0').should.eventually.deep.equal(['www.video.com']));
   });
 });
 
 describe('incScore', () => {
   it('should increment a video\'s score', () => {
-    redisController.incScore('Room 1', 'www.video.com');
-    return client.zscoreAsync('Room 1videos', 'www.video.com').should.eventually.equal('1');
+    return redisController.incScore('Room 1', 'www.video.com')
+      .then(() => client.zscoreAsync('Room 1videos', 'www.video.com').should.eventually.equal('1'))
+  });
+  it('should return a number with the video\'s new score after incrementing', () => {
+    return redisController.incScore('Room 1', 'www.video.com').should.eventually.equal(2);
   });
 });
 
 describe('returnVideo', () => {
   it('should return the video in first place as a string', () => {
-    client.zaddAsync('Room 1videos', 0, 'www.video2.com')
-    return client.zrangeAsync('Room 1videos', 0, 2)
+    return client.zaddAsync('Room 1videos', 0, 'www.video2.com')
       .then(() => {
-        return redisController.returnVideo('Room 1').should.eventually.equal('www.video2.com');
+        return redisController.returnVideo('Room 1').should.eventually.equal('www.video.com');
       });
   });
 
   it('should remove the returned video from the sorted set', () => {
-    return client.zscoreAsync('Room 1videos', 'www.video.com').should.eventually.equal('1');
+    return client.zscoreAsync('Room 1videos', 'www.video.com').should.eventually.equal(null);
   })
 
   it('should get the next highest ranked video after removing the higest ranked video', () => {
-    return redisController.returnVideo('Room 1').should.eventually.equal('www.video.com');
+    return redisController.returnVideo('Room 1').should.eventually.equal('www.video2.com');
   });
 });
 
@@ -86,7 +88,26 @@ describe('returnQueue', () => {
         return redisController.returnQueue('Room 1').should.eventually.deep.equal(['www.video1.com', 'www.video2.com', 'www.video3.com']);
       })
   });
+
   it('should return an array', () => {
     return redisController.returnQueue('Room 1').then(resp => Array.isArray(resp)).should.be.ok;
+  })
+
+  xdescribe('removeFromQueue', () => {
+    it('should remove a video from the queue', () => {
+      return redisController.removeFromQueue('www.video3.com')
+        .then(client.zscoreAsync('Room 1videos', 'www.video3.com').should.eventually.equal(null));
+    });
+    it('should throw an error if the provided url is not in the queue', () => {
+      return redisController.removeFromQueue('www.videothatdoesnotexisst.com')
+        .then(client.zscoreAsync('Room 1videos', 'www.video3.com').should.be.rejectedWith('provided url does not exist in queue'));
+    });
+  })
+
+  xdescribe('removeFromQueue', () => {
+    it('should remove a video from the queue', () => {
+      return redisController.removeFromQueue('www.video3.com')
+        .then(client.zscoreAsync('Room 1videos', 'www.video3.com').should.eventually.equal(null));
+    });
   })
 });
