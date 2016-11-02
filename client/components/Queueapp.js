dd2436f9e38daafa136ace0a77a3ec9444ec44d5
David Wilson <w.dave.w@gmail.com>
2016-11-01 22:48:06 -0700

    Queue working again

--- client/components/Queueapp.js
+++ client/components/Queueapp.js
@@ -4,6 +4,7 @@ import Queue from './Queue';
 import Form from './Queueform';
 import QueueList from './Queuelist';
 import io from 'socket.io-client';
+import ReactPlayer from 'react-player'
 
 const HOST = "http://localhost:3000";
 
@@ -15,7 +16,7 @@ class QueueApp extends Component {
       video: ''
     }
     this.socket = io.connect(HOST);
-
+    this.admin = this.userIsAdmin();
     this.getData = this.getData.bind(this);
     this.formClick = this.formClick.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
@@ -24,87 +25,108 @@ class QueueApp extends Component {
   /**
    * We GET our data here after each render
    */
+
+  userIsAdmin() {
+    return localStorage.getItem(`admin${this.props.params.roomName}`)
+  }
+
   getData() {
-    console.log('received message to get new data');
-    $.get(HOST + `/queue/${this.props.params.roomName}`).done(data => this.setState({ queues: data }));
+    if (this.state.video === '' && this.admin) {
+        $.ajax({
+          type: "GET",
+          url: HOST + `/getNextVideo/${this.props.params.roomName}`,
+          contentType: "application/json; charset=utf-8",
+        }).done((response) => {
+          this.setState({ video: response.split('=')[1] });
+          this.socket.emit('refreshQueue', { room: this.props.params.roomName });
+        })
+      } else {
+      $.get(HOST + `/queue/${this.props.params.roomName}`).done(data => this.setState({ queues: data }));
+    }
   }
-  /**
- * handleStateChange is an event listener for the react-youtube
- * component's state. The states are as follows:
- * UNSTARTED: -1, ENDED: 0, PLAYING: 1, PAUSED: 2, BUFFERING: 3, CUED: 5
+
+/**
+* handleStateChange is an event listener for the react-youtube
+* component's state. The states are as follows:
+* UNSTARTED: -1, ENDED: 0, PLAYING: 1, PAUSED: 2, BUFFERING: 3, CUED: 5
+*/
+handleStateChange(event) {
+  // CUED was a good option for enabling "auto play" because it waits
+  // until the player is loaded (-1) and then the video is cued ready to play.
+  // ENDED allows repeat behavior for last video.
+  if (event.data === 5) event.target.playVideo();
+  if (event.data === 2 && this.admin) this.socket.emit('pauseVideo')
+  if (event.data === 1 && this.admin) this.socket.emit('playVideo')
+}
+/**
+ * TODO: get access to url that the admin wants to remove
  */
-  handleStateChange(event) {
-    // CUED was a good option for enabling "auto play" because it waits
-    // until the player is loaded (-1) and then the video is cued ready to play.
-    // ENDED allows repeat behavior for last video.
-    if (event.data === 5) event.target.playVideo();
-  }
-  /**
-   * TODO: get access to url that the admin wants to remove
-   */
 
-  
-  handlePlayerEnd(event) {
-    console.log('local storage', (localStorage.getItem(`admin${this.props.params.roomName}`)));
-    if (localStorage.getItem(`admin${this.props.params.roomName}`)) {
-      $.ajax({
-        type: "GET",
-        url: HOST + `/getNextVideo/${this.props.params.roomName}`,
-        contentType: "application/json; charset=utf-8",
-      }).done((response) => {
-        this.setState({ video: response });
-        this.socket.emit('refreshQueue', {room: this.props.params.roomName});
-    });
-    }
-  }
-  /**
-   * This is the callback for the form component to use in onClick.
-   * It makes an ajax request to add a new link when the submit button is clicked.
-   */
-  formClick(link) {
-    // TODO this functionality should be replaced with socket logic.
-    // let newQueues = [...this.state.queues];
-    // newQueues.push(link);
-    // this.setState({ queues: newQueues });
+
+handlePlayerEnd(event) {
+  if (this.admin) {
     $.ajax({
-      url: HOST + '/addToQueue',
-      type: "POST",
-      data: JSON.stringify({ link: link, room: this.props.params.roomName }),
+      type: "GET",
+      url: HOST + `/getNextVideo/${this.props.params.roomName}`,
       contentType: "application/json; charset=utf-8",
-      dataType: "json",
-    }).done(() => this.socket.emit('newdata'))
+    }).done((response) => {
+      this.setState({ video: response.split('=')[1]  });
+      this.socket.emit('refreshQueue', { room: this.props.params.roomName });
+    });
   }
-  /**
-   * This is where the listeners for this.socket go.
-   * on[newData] -> Implies there is a change in data on the backend.
-   *                The callback will make a GET request and update state
-   *                with the new list of Youtube URLs.
-   * on[joiningRoom] -> Implies someone has created a room.
-   *                    The callback will update our roomName in the constructor
-   *                    with the input value typed into the Home createRoom form.
-   */
-  componentDidMount() {
-    this.getData();
-    this.socket.emit('room', {roomName: this.props.params.roomName});
-    this.socket.on('newdata', this.getData);
-  }
-
-  render() {
-    let videoUrl;
-    if (this.state.video) {
-      videoUrl = this.state.video;
-      console.log(videoUrl);
-    }
-
-    return (
-      <div className="youtube-wrapper">
-        <h1>Welcome to QTube! Your room is: {this.props.params.roomName}</h1>
-        <Form key={'form-key'} formClick={this.formClick} />
-        <Youtube videoId={videoUrl} onEnd={this.handlePlayerEnd} onStateChange={this.handleStateChange} />
-        <QueueList queues={this.state.queues} />
-      </div>
-    )
+}
+/**
+ * This is the callback for the form component to use in onClick.
+ * It makes an ajax request to add a new link when the submit button is clicked.
+ */
+formClick(link) {
+  // TODO this functionality should be replaced with socket logic.
+  // let newQueues = [...this.state.queues];
+  // newQueues.push(link);
+  // this.setState({ queues: newQueues });
+  $.ajax({
+    url: HOST + '/addToQueue',
+    type: "POST",
+    data: JSON.stringify({ link: link, room: this.props.params.roomName }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+  }).done(() => this.socket.emit('newdata'))
+}
+/**
+ * This is where the listeners for this.socket go.
+ * on[newData] -> Implies there is a change in data on the backend.
+ *                The callback will make a GET request and update state
+ *                with the new list of Youtube URLs.
+ * on[joiningRoom] -> Implies someone has created a room.
+ *                    The callback will update our roomName in the constructor
+ *                    with the input value typed into the Home createRoom form.
+ */
+componentDidMount() {
+  this.getData();
+  this.socket.emit('room', { roomName: this.props.params.roomName });
+  this.socket.on('newdata', this.getData);
+  this.socket.on('pauseVideo', () => {
+    this.
   }
 }
 
+render() {
+  let videoUrl;
+  if (this.state.video) {
+    videoUrl = this.state.video;
+  }
+  console.log(this.state.queues);
+
+  return (
+    <div className="youtube-wrapper">
+      <h1>Welcome to QTube! Your room is: {this.props.params.roomName}</h1>
+      <Form key={'form-key'} formClick={this.formClick} />
+      <Youtube videoId={videoUrl} onEnd={this.handlePlayerEnd} onStateChange={this.handleStateChange} />
+      <QueueList queues={this.state.queues} />
+    </div>
+  )
+}
+    }
+
 export default QueueApp;
+ 
--- client/components/Queuelist.js
+++ client/components/Queuelist.js
@@ -4,8 +4,7 @@ import Queue from './queue';
 const QueueList = ({ queues }) => {
   // Create Queue component for URLs. Format validation moved to server.
   // Slice at 1 to avoid showing thumbnail for current video playing.
-  const validUrls = queues.slice(1).map((queue, index) => <Queue key={index} link={queue} />);
-
+  const validUrls = queues.map((queue, index) => <Queue key={index} link={queue} />);
   return <div>{validUrls}</div>;
 };
 
