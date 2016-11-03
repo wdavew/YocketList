1670767586b11f4f6cd1e69104787fc1282e43cd
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-02 19:43:20 -0700

    added id for styling

--- client/components/Queue.js
+++ client/components/Queue.js
@@ -9,7 +9,7 @@ class Queue extends Component {
   // For src: this interesting piece of string manipulation takes in a youtube url and turns it into a thumbnail
   render() {
     return (
-      <div>
+      <div id="eachImg">
         <img className="images" onDoubleClick={() => this.props.thumbnailClick(this.props.link)} src={`https://i.ytimg.com/vi/${this.props.link.split('=')[1]}/hqdefault.jpg`}></img>;
         <p>{this.props.score}</p>
       </div>
